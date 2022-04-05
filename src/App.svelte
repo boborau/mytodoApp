fb6c9d806794a854e17bdef062133f72bb5c4242
boborau <brau1@confederationcollege.ca>
2022-04-05 13:56:02 -0400

    initial push

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+	import Todo from './Todo.svelte';
+</script>
+
+<main>
+	<h1>REY MYSTERIO'S TO DO LIST!</h1>
+	<p> WELCOME TO REY MYSTERIO'S <span class="superCool">SUPER COOL</span> TO DO LIST! BOOYAKA BOOYAKA</p>
+	<Todo />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	p {
+		color: black;
+		font-size: 2rem;
+	}
+
+	h1 {
+		color: black;
+		font-weight: bolder;
+		text-transform: uppercase;
+		font-size: 5rem;
+		border-style: double;
+		border-width: 20px;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Items.svelte
@@ -0,0 +1,17 @@
+<script>
+     export let text;
+     export let checked;
+     export let removeItem;
+     export let toggleDone;
+</script>
+
+<li><input bind:checked={checked} on:change={toggleDone} type="checkbox"> 
+     <span class:checked={checked}>{text}</span>
+     <button type="button" on:click={removeItem}>BOOYAKA BOOYAKA 619</button>
+</li>
+
+<style>
+     .checked {
+          text-decoration: line-through;
+     }
+</style>
